e4e03e948b420285bdee8838e9f18e081597c78a
yawlad33 <yawlad33@gmail.com>
2021-06-27 11:58:40 +0300

    Laba1-1
    Laba1-1

---
+++ My_1_1/Source.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Rus");
+	const int N = 10;
+	int arr[N];
+
+	cout << "¬ведите массив из " << N  << " элементов: ";
+	for (int i = 0; i < N; i++)
+		cin >> arr[i];
+
+	for (int i = 0; i < N; i++)
+	{
+		if (arr[i] % 2 == 0)
+			arr[i] = 0;
+	}
+
+	for (int i = 0; i < N; i++)
+		cout << arr[i] << " ";
+
+	return 0;
+}
